1069d1d92f812b38fda538f4f1b131415f3c0bed
seud <seudsahm1@gmail.com>
2025-02-01 23:50:23 +0300

    add the app to installed apps list and create a local urls.py file

--- e_shop/settings.py
+++ e_shop/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third part apps
     'rest_framework',
+
+    # user defined apps
+    'stores',
 ]
 
 MIDDLEWARE = [
