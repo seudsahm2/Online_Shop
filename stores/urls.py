7254337de4a0d2cb4fe0777d3f84cefaefbd2818
seud <seudsahm1@gmail.com>
2025-02-02 23:47:58 +0300

    change views to ViewSets and update urls based on that

--- stores/urls.py
+++ stores/urls.py
@@ -1,31 +1,48 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 from . import views
-
+from pprint import pprint
 urlpatterns = [
-    path('', views.StoreListAPIView.as_view(), name="store_list"),
-    path('<int:id>/', views.StoreDetailAPIView.as_view(), name="store_detail"),
-    path('create/', views.StoreCreateAPIView.as_view(), name="create_store"),
-    path('update/<int:id>/', views.StoreRetrieveUpdateAPIView.as_view(),
-         name="update_store"),
-    path('delete/<int:id>/', views.StoreDestroyAPIView.as_view(), name="delete_store"),
+    # path('', views.StoreListAPIView.as_view(), name="store_list"),
+    # path('<int:id>/', views.StoreDetailAPIView.as_view(), name="store_detail"),
+    # path('create/', views.StoreCreateAPIView.as_view(), name="create_store"),
+    # path('update/<int:id>/', views.StoreRetrieveUpdateAPIView.as_view(),
+    #      name="update_store"),
+    # path('delete/<int:id>/', views.StoreDestroyAPIView.as_view(), name="delete_store"),
 
-    path('products/', views.ProductListAPIView.as_view(), name="products"),
-    path('products/<int:id>/', views.ProductDetailAPIView.as_view(),
-         name="product_detail"),
-    path('products/create/', views.ProductCreateAPIView.as_view(),
-         name="create_product"),
-    path('products/update/<int:id>/', views.ProductRetrieveUpdateAPIView.as_view(),
-         name="update_product"),
-    path('products/delete/<int:id>/',
-         views.ProductDestroyAPIView.as_view(), name="delete_product"),
+    # path('products/', views.ProductListAPIView.as_view(), name="products"),
+    # path('products/<int:id>/', views.ProductDetailAPIView.as_view(),
+    #      name="product_detail"),
+    # path('products/create/', views.ProductCreateAPIView.as_view(),
+    #      name="create_product"),
+    # path('products/update/<int:id>/', views.ProductRetrieveUpdateAPIView.as_view(),
+    #      name="update_product"),
+    # path('products/delete/<int:id>/',
+    #      views.ProductDestroyAPIView.as_view(), name="delete_product"),
 
-    path('categories/', views.CategoryListAPIView.as_view(), name="category_list"),
-    path('categories/<int:id>/', views.CategoryDetailAPIView.as_view(),
-         name="product_detail"),
-    path('categories/create/', views.CategoryCreateAPIView.as_view(),
-         name="create_product"),
-    path('categories/update/<int:id>/', views.CategoryRetrieveUpdateAPIView.as_view(),
-         name="update_product"),
-    path('categories/delete/<int:id>/',
-         views.CategoryDestroyAPIView.as_view(), name="delete_product"),
+    # path('categories/', views.CategoryListAPIView.as_view(), name="category_list"),
+    # path('categories/<int:id>/', views.CategoryDetailAPIView.as_view(),
+    #      name="product_detail"),
+    # path('categories/create/', views.CategoryCreateAPIView.as_view(),
+    #      name="create_product"),
+    # path('categories/update/<int:id>/', views.CategoryRetrieveUpdateAPIView.as_view(),
+    #      name="update_product"),
+    # path('categories/delete/<int:id>/',
+    #      views.CategoryDestroyAPIView.as_view(), name="delete_product"),
 ]
+
+router = DefaultRouter()
+router.register('', views.StoreViewSet)
+
+product_router = routers.NestedSimpleRouter(router, '', lookup='store')
+product_router.register('products', views.ProductViewSet,
+                        basename='store-products')
+
+category_router = routers.NestedSimpleRouter(router, '', lookup='store')
+product_router.register('category', views.CategoryViewSet,
+                        basename='store-category')
+
+urlpatterns += router.urls + product_router.urls
+pprint(router.urls)
+pprint(product_router.urls)
--- stores/views.py
+++ stores/views.py
@@ -1,86 +1,103 @@
 from django.shortcuts import render
 from .serializers import StoreListSerializer, StoreDetailSerializer, ProductListSerializer, ProductDetailSerializer, CategoryListSerializer, CategoryDetailSerializer
 from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
 from .models import Store, Category, Product
 # Create your views here.
 
 
-class CategoryListAPIView(generics.ListAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategoryListSerializer
+# class CategoryListAPIView(generics.ListAPIView):
+#     queryset = Category.objects.all()
+#     serializer_class = CategoryListSerializer
 
 
-class CategoryDetailAPIView(generics.RetrieveAPIView):
-    lookup_field = "id"
+# class CategoryDetailAPIView(generics.RetrieveAPIView):
+#     lookup_field = "id"
+#     queryset = Category.objects.all()
+#     serializer_class = CategoryDetailSerializer
+
+
+# class CategoryCreateAPIView(generics.CreateAPIView):
+#     queryset = Category.objects.all()
+#     serializer_class = CategoryDetailSerializer
+
+
+# class CategoryRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+#     lookup_field = "id"
+#     queryset = Category.objects.all()
+#     serializer_class = CategoryDetailSerializer
+
+
+# class CategoryDestroyAPIView(generics.DestroyAPIView):
+#     lookup_field = "id"
+#     queryset = Category.objects.all()
+    
+class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategoryDetailSerializer
 
 
-class CategoryCreateAPIView(generics.CreateAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategoryDetailSerializer
+# class StoreListAPIView(generics.ListAPIView):
+#     queryset = Store.objects.all()
+#     serializer_class = StoreListSerializer
 
 
-class CategoryRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
-    lookup_field = "id"
-    queryset = Category.objects.all()
-    serializer_class = CategoryDetailSerializer
+# class StoreDetailAPIView(generics.RetrieveAPIView):
+#     lookup_field = "id"
+#     queryset = Store.objects.all()
+#     serializer_class = StoreDetailSerializer
 
 
-class CategoryDestroyAPIView(generics.DestroyAPIView):
-    lookup_field = "id"
-    queryset = Category.objects.all()
+# class StoreCreateAPIView(generics.CreateAPIView):
+#     queryset = Store.objects.all()
+#     serializer_class = StoreDetailSerializer
 
 
-class StoreListAPIView(generics.ListAPIView):
-    queryset = Store.objects.all()
-    serializer_class = StoreListSerializer
+# class StoreRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+#     lookup_field = "id"
+#     queryset = Store.objects.all()
+#     serializer_class = StoreDetailSerializer
 
 
-class StoreDetailAPIView(generics.RetrieveAPIView):
-    lookup_field = "id"
-    queryset = Store.objects.all()
-    serializer_class = StoreDetailSerializer
+# class StoreDestroyAPIView(generics.DestroyAPIView):
+#     lookup_field = "id"
+#     queryset = Store.objects.all()
 
 
-class StoreCreateAPIView(generics.CreateAPIView):
-    queryset = Store.objects.all()
-    serializer_class = StoreDetailSerializer
-
-
-class StoreRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
-    lookup_field = "id"
+class StoreViewSet(ModelViewSet):
     queryset = Store.objects.all()
     serializer_class = StoreDetailSerializer
 
 
-class StoreDestroyAPIView(generics.DestroyAPIView):
-    lookup_field = "id"
-    queryset = Store.objects.all()
+# class ProductListAPIView(generics.ListAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductListSerializer
 
 
-class ProductListAPIView(generics.ListAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductListSerializer
+# class ProductDetailAPIView(generics.RetrieveAPIView):
+#     lookup_field = "id"
+#     queryset = Product.objects.all()
+#     serializer_class = ProductDetailSerializer
 
 
-class ProductDetailAPIView(generics.RetrieveAPIView):
-    lookup_field = "id"
-    queryset = Product.objects.all()
+# class ProductCreateAPIView(generics.CreateAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductDetailSerializer
+
+
+# class ProductRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+#     lookup_field = "id"
+#     queryset = Product.objects.all()
+#     serializer_class = ProductDetailSerializer
+
+
+# class ProductDestroyAPIView(generics.DestroyAPIView):
+#     lookup_field = "id"
+#     queryset = Product.objects.all()
+
+class ProductViewSet(ModelViewSet):
+    # queryset = Product.objects.all()
     serializer_class = ProductDetailSerializer
 
-
-class ProductCreateAPIView(generics.CreateAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductDetailSerializer
-
-
-class ProductRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
-    lookup_field = "id"
-    queryset = Product.objects.all()
-    serializer_class = ProductDetailSerializer
-
-
-class ProductDestroyAPIView(generics.DestroyAPIView):
-    lookup_field = "id"
-    queryset = Product.objects.all()
+    def get_queryset(self):
+        return Product.objects.filter(store_id=self.kwargs['store_pk'])
